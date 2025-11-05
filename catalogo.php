be4ba3a97e839b691cc321276847c85811f5a8cf
ProfAlbertoAyres <alberto.benicio@ifro.edu.br>
2025-11-05 08:35:55 -0400

    Configuração do Catalogo

--- catalogo.php
+++ catalogo.php
@@ -16,64 +16,92 @@
     <main class="container mt-5">
         <h2 class="text-center mb-5">Conheça Nossos Animais</h2>
         <div class="d-flex gap-4 justify-content-center flex-wrap">
+            <!-- Inicia o laço dos animais  -->
+            <?php
+                spl_autoload_register(function ($class) {
+                    require_once"classes/{$class}.class.php";
+                });
+                $animal = new Animal();
+                $foto = new FotoAnimal();
+                $animais = $animal->sp_exibir("exibir_animais");
+                foreach ($animais as  $anm) : #Abertura do laço
+            ?>
             <div class="card">
                 <!-- Imagens -->
-
-                <div id="carouselAnimal-" class="carousel slide">
+                 <?php
+                    $fotos = $foto->fotosAnimal($anm->id_animal);
+                    $primeira = true;
+                    if(count($fotos) > 0):
+                        ?>
+                <div id="carouselAnimal-<?=$anm->id_animal?>" class="carousel slide">
                     <div class="carousel-inner">
-
-                        <div class="carousel-item ">
-                            <img src="uploads/" class="d-block mx-auto" alt="...">
+                        <?php
+                        foreach ($fotos as $ft):
+                            ?>
+                        <div class="carousel-item <?= $primeira ? 'active':null?>">
+                            <img src="uploads/<?=$ft->nome?>" class="d-block mx-auto" alt="<?=$ft->alternativo?>">
                         </div>
-
+                            <?php
+                            $primeira = false;
+                            endforeach;
+                            ?>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselAnimal-"
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselAnimal-<?= $anm->id_animal?>"
                         data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselAnimal-"
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselAnimal-<?= $anm->id_animal?>"
                         data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
+                <?php
+                    else:;
+                    ?>
                 <img src="uploads/semFoto.png" alt="Animal sem foto" class="semFotoC">
+                <?php endif; ?>
                 <div class="card-body">
-                    <h5 class="card-title"></h5>
+                    <h5 class="card-title"><?= $anm->identificdor?></h5>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Nascimento: </li>
-                    <li class="list-group-item"></li>
-                    <li class="list-group-item"></li>
+                    <li class="list-group-item">Nascimento: <?= $anm->nascimento ?></li>
+                    <li class="list-group-item"><?= $anm->sexo ?></li>
+                    <li class="list-group-item"><?= $anm->lote?></li>
                 </ul>
                 <div class="card-body">
                     <!-- Button trigger modal -->
                     <button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal"
-                        data-bs-target="#animalModal-">
+                        data-bs-target="#animalModal-<?=$anm->id_animal ?>">
                         Mais informações
                     </button>
                 </div>
             </div>
+            <!-- Finaliza o laço dos animais -->
+                <?php endforeach; ?>
         </div>
-
-        <div class="modal fade" id="animalModal-" tabindex="-1" aria-labelledby="animalModalLabel-" aria-hidden="true">
+                    <?php 
+                    foreach($animais as $anm):
+                     ?>
+        <div class="modal fade" id="animalModal-<?=$anm->id_animal ?>" tabindex="-1" aria-labelledby="animalModalLabel-<?=$anm->id_animal ?>" aria-hidden="true">
             <div class="modal-dialog modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="animalModalLabel-">Animal: </h1>
+                        <h1 class="modal-title fs-5" id="animalModalLabel-<?=$anm->id_animal ?>">Animal: <?=$anm->identificdor ?></h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <p>Sexo:</p>
-                        <p class="text-justify">Nascimento:</p>
-                        <p class="text-justify">Raça: </p>
-                        <p class="text-justify">Lote: </p>
+                        <p>Sexo: <?=$anm->sexo ?></p>
+                        <p class="text-justify">Nascimento: <?=$anm->nascimento ?></p>
+                        <p class="text-justify">Raça: <?=$anm->raca ?></p>
+                        <p class="text-justify">Lote: <?=$anm->lote ?></p>
 
                     </div>
                 </div>
             </div>
         </div>
+        <?php endforeach; ?>
     </main>
     <?php require_once "_parts/_footer.php" ?>
 
